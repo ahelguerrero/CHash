f2fe01b940c4cdf42a379d81a0d41cc75785611d
Ahel <Ahel@DESKTOP-C29K24J>
2019-08-11 01:09:44 -0500

    Add main and ReadMe

---
+++ CHash/scr/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+void start(){
+
+}
+void print((void*) line){
+
+}
+int main(int args, char*[] argvs){
+  
+  printf("%s","Hello word");
+  printf("%s","Starting Application");
+  return 0;
+}
